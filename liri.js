1084c998fee0121eaa093e79e6bdabba3a6ab2b9
phild6596 <phild6596@gmail.com>
2018-01-31 13:43:55 -0800

    started adding all the 'require' javascript var's.

--- liri.js
+++ liri.js
@@ -1,1 +1,20 @@
 require("dotenv").config();
+var fs = require('fs');
+var spotify = require('spotify');
+var twitter = require('twitter');
+var request = require('request');
+var keys = require('./keys.js');
+
+//here is the function to call twitter
+
+
+//here is the function to call spotify
+
+
+//here is the function to call OMDB
+
+
+//here is the what it says function
+
+
+//here is the write to log function
