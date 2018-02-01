d516c9f74a1a4315752e48df0732c6ccb1cd1b50
phild6596 <phild6596@gmail.com>
2018-02-01 09:49:09 -0800

    created Twitter function and node can now print that to console.

--- liri.js
+++ liri.js
@@ -8,8 +8,8 @@ var keys = require('./keys.js');
 var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
 
-console.log(client);
-console.log(spotify);
+//console.log(client);
+//console.log(spotify);
 //Write to random.txt
 var randomTxt = function (data) {
     fs.appendFile('random.txt', '\r\n\r\n');
@@ -21,14 +21,42 @@ var randomTxt = function (data) {
     });
 }
 
+//here is the function to call Twitter
+var tweets = function () {
+    var parameters = { screen_name: 'Phlip_Deez', count:10 };
 
-//here is the function to call spotify
-
-
+    client.get('statuses/user_timeline', parameters , function (error, tweets, response){
+        var data = [];
+        for (let i = 0; i < tweets.length; i++) {
+            var newTweets = tweets[i];
+            //console.log(newTweets);
+            data.push({
+                'Created at': tweets[i].created_at,
+                'Tweets ': tweets[i].text,
+            });
+                if (error) {
+                return error;
+                }
+        }
+        console.log(data);
+    })
+}
 //here is the function to call OMDB
 
 
 //here is the what it says function
 
 
-//here is the write to log function
+//here is where the arguments are called
+var pick = function (caseData, functionData) {
+    switch (caseData) {
+        case 'my-tweets':
+        tweets();
+        break;
+    }
+}
+
+var runLiri = function (argumentOne, arguementTwo) {
+    pick(argumentOne, arguementTwo);
+};
+runLiri(process.argv[2], process.argv[3]);
